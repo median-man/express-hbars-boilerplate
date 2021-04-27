4dd691c9a1e1bc13b8a8cbd31dbfc0e06d43878f
John Desrosiers <john.desro@gmail.com>
2021-04-27 14:26:09 -0700

    Fix server startup

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
 require("dotenv").config();
-const util = require("util");
 const express = require("express");
 const sequelize = require("./config/connection");
+const router = require("./controllers");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const listen = util.promisify(app.listen);
+
+// setup app middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(router);
+
+// connect db and listen
 sequelize
   .sync({ force: false })
-  .then(() => listen())
-  .then(() => console.log(`App listening on PORT ${PORT}`))
+  .then(() => {
+    app.listen(PORT, (err) => {
+      if (err) {
+        console.error(err);
+        return process.exit(1);
+      }
+      console.log(`App listening on PORT ${PORT}`);
+    });
+  })
   .catch((err) => {
     console.error(err);
     process.exit(1);
