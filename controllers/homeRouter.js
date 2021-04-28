56512d0a76a4e4f38b1c7112cf3960fa225b591f
John Desrosiers <john.desro@gmail.com>
2021-04-28 07:32:03 -0700

    Users can login and signup

--- controllers/homeRouter.js
+++ controllers/homeRouter.js
@@ -1,11 +1,32 @@
 const router = require("express").Router();
+const { User } = require("../models");
 
-router.get("/", (req, res) => {
-  res.render("home", {
-    title: "Home Page",
-    isLoggedIn: false,
-    user: { username: "test", id: 1 },
-  });
+// use withAuth middleware to redirect from protected routes.
+// const withAuth = require("../util/withAuth");
+
+// example of a protected route
+// router.get("/users-only", withAuth, (req, res) => {
+//   // ...
+// });
+
+router.get("/", async (req, res) => {
+  try {
+    let user;
+    if (req.session.isLoggedIn) {
+      user = await User.findByPk(req.session.userId, {
+        exclude: ["password"],
+        raw: true,
+      });
+    }
+    res.render("home", {
+      title: "Home Page",
+      isLoggedIn: req.session.isLoggedIn,
+      user,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("⛔ Uh oh! An unexpected error occurred.");
+  }
 });
 
 router.get("/login", (req, res) => {
